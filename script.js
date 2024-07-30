3da00e1b36fffeaed937fe36387bac65b710084f
GezaKassay <kassay_geza@yahoo.com>
2024-07-30 13:10:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-let fighterJet = document.createElement('img');
+let fighterJet = document.createElement("img");
 fighterJet.style.width = "50px"; 
 fighterJet.style.height = "50px";
 fighterJet.src = "vector-fighter-jet-icon-design.jpg";
-document.getElementById('158').appendChild(fighterJet);
+document.getElementById("158").appendChild(fighterJet);
 
-let asteroid = document.createElement('img');
+let asteroid = document.createElement("img");
 asteroid.style.width = "50px"; 
 asteroid.style.height = "50px";
 asteroid.src = "asteroid-icon-flat-style-vector.jpg";
-document.getElementById('143').appendChild(asteroid);
+document.getElementById("143").appendChild(asteroid);
+
+let maxLeft = 151;
+let maxRight = 165;
+let jetLocation = 158;
+
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+        return;
+    }
+  
+    switch (event.key) {      
+        case "ArrowLeft":
+            if (jetLocation > maxLeft) {
+                --jetLocation;
+                document.getElementById(jetLocation).appendChild(fighterJet);
+            }        
+            break;
+        case "ArrowRight":
+            if (jetLocation < maxRight) {
+                ++jetLocation;
+                document.getElementById(jetLocation).appendChild(fighterJet);
+            }
+            break;
+        default:
+            return;
+    }
+  
+    event.preventDefault();
+  }, true);
