06cf9623b79754233697f77e1727fb34d38a62bb
GezaKassay <kassay_geza@yahoo.com>
2024-07-30 10:00:05 +0300

    initial work

---
+++ script.js
@@ -0,0 +1,11 @@
+let fighterJet = document.createElement('img');
+fighterJet.style.width = "50px"; 
+fighterJet.style.height = "50px";
+fighterJet.src = "vector-fighter-jet-icon-design.jpg";
+document.getElementById('158').appendChild(fighterJet);
+
+let asteroid = document.createElement('img');
+asteroid.style.width = "50px"; 
+asteroid.style.height = "50px";
+asteroid.src = "asteroid-icon-flat-style-vector.jpg";
+document.getElementById('143').appendChild(asteroid);
